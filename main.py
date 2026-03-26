f83964cf5ab9f3a0c8608c5869f6dd03b9436a8d
aditya4w <aditya4core@gmail.com>
2026-03-26 20:09:53 +0530

    Add advance math function - coming soon.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
+import math
+
+# For clean
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+# For asthetics
 
 def border():
     print("╔══════════════════════╗")
@@ -15,6 +20,7 @@ def end():
 def line():
     print("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
 
+# Secondary Menus
 
 def SecMenu():
     while True:
@@ -36,6 +42,63 @@ def SecMenu():
         except ValueError:
             print("Enter valid option.")
 
+def AdvMath():
+    while True:
+        clear()
+        border()
+        print("║    Advance Maths     ║")
+        mid()
+        print(" 1. Factorial")
+        print(" 2. Square")
+        print(" 3. Cube")
+        print(" 4. Root")
+        print(" 5. Log")
+        print(" 6. Back/Exit")
+        end()
+
+        try:
+            choice = int(input("What would you like to do? \n :"))
+
+            match choice:
+                case 1:
+                    line()
+                    num = int(input("Enter a number: "))
+                    line()
+                    while num < 0:
+                        print("Factorial of negative number doesn't exit.")
+                        line()
+                        num = int(input("Enter a number :"))
+    
+                    line()
+                    Factorial(num)
+                    result = SecMenu()
+                    if result == 2:
+                        return 2
+
+
+                case 2:
+                    pass
+
+                case 3:
+                    pass
+                
+                case 4:
+                    pass
+
+                case 5:
+                    pass
+
+                case 6:
+                    pass
+
+        except ValueError:
+            print("Enter a valid option.")
+
+
+
+
+# Math Func
+
 def Add(num1, num2):
      print(f"{num1} + {num2} = {num1 + num2}.")
 
@@ -46,7 +109,29 @@ def Mult(num1, num2):
     print(f"{num1} × {num2} = {num1 * num2}.")
 
 def Div(num1, num2):
-    print(f"{num1} / {num2} = {num1 / num2}")
+    print(f"{num1} / {num2} = {num1 / num2}.")
+
+# Advance Math Func
+
+def Factorial(num):
+    print(f"{num}! = {math.factorial(num)}.")
+
+def Root(num):
+    print(f"√{num} = {math.sqrt(num)}.")
+
+def Square(num):
+    num_sqr = num ** 2
+    print(f"{num}² = {num_sqr}.")
+
+def Cube(num):
+    num_cube = num ** 3
+    print(f"{num}³ = {num_cube}.")
+
+def Log(num):
+    print(f"Log{num} to base e = {math.log(num)}.")
+    print(f"Log{num} to base 10 = {math.log10(num)}.")
+
+# Main function
 
 def main():
     while True:
